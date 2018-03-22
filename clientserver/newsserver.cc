510d85021a4f86fdfe161b69e631aef4edc08c00
antongoransson <anton.goransson95@gmail.com>
2018-03-22 16:36:57 +0100

    Add removed stuff

--- clientserver/newsserver.cc
+++ clientserver/newsserver.cc
@@ -9,6 +9,8 @@
 #include <string>
 #include <stdexcept>
 #include <cstdlib>
+#include "databaseinterface.h"
+#include "database.h"
 
 using std::cout;
 using std::endl;
@@ -20,7 +22,7 @@ using std::make_shared;
 using std::string;
 
 
-void handle_list_articles(MessageHandler& mh) {
+void handle_list_articles(MessageHandler& mh, DatabaseInterface& db) {
 	 mh.send_code(Protocol::ANS_LIST_ART);
 	 int nbr = mh.recv_int_parameter();
 	 // pair<vector<pair<int,string>, bool> articles = db.list_articles();
@@ -33,10 +35,10 @@ void handle_list_articles(MessageHandler& mh) {
 	 mh.send_code(Protocol::ANS_END);
 }
 
-void get_list_newsgroups(MessageHandler& mh){
+void get_list_newsgroups(MessageHandler& mh, DatabaseInterface& db){
 	mh.send_code(Protocol::ANS_LIST_NG);
-	vector<Pair<int,string> v = list_news_groups();
-	int size = v.length();
+	vector<pair<int,string>> v = db.list_news_groups();
+	int size = v.size();
 	mh.send_int_parameter(size);
 	for(int i = 0; i < size; i++){
 		mh.send_int_parameter(v[i].first);
@@ -45,9 +47,9 @@ void get_list_newsgroups(MessageHandler& mh){
 	mh.send_code(Protocol::ANS_END);
 }
 
-void create_newsgroup(MessageHandler& mh){
+void create_newsgroup(MessageHandler& mh, DatabaseInterface& db){
 	mh.send_code(Protocol::ANS_CREATE_NG);
-	if(create_news_group(mh.recv_string_parameter())){
+	if(db.create_news_group(mh.recv_string_parameter())){
 		mh.send_code(Protocol::ANS_ACK);
 	}
 	else{
@@ -57,9 +59,9 @@ void create_newsgroup(MessageHandler& mh){
 	mh.send_code(Protocol::ANS_END);
 }
 
-void delete_newsgroup(MessageHandler& mh){
+void delete_newsgroup(MessageHandler& mh, DatabaseInterface& db){
 	mh.send_code(Protocol::ANS_DELETE_NG);
-	if(delete_news_group(mh.recv_int_parameter())){
+	if(db.delete_news_group(mh.recv_int_parameter())){
 		mh.send_code(Protocol::ANS_ACK);
 	}
 	else{
@@ -69,12 +71,12 @@ void delete_newsgroup(MessageHandler& mh){
 	mh.send_code(Protocol::ANS_END);
 }
 
-void handle_create_article(MessageHandler& mh){
+void handle_create_article(MessageHandler& mh, DatabaseInterface& db){
 // COM_CREATE_ART num_p string_p string_p string_p COM_END
 // ANS_CREATE_ART [ANS_ACK | ANS_NAK ERR_NG_DOES_NOT_EXIST] ANS_END
 
 	mh.send_code(Protocol::ANS_CREATE_ART);
-	if(create_article(mh.recv_string_parameter(), mh.recv_string_parameter(), mh.recv_string_parameter())){
+	if(db.create_article(mh.recv_int_parameter(), mh.recv_string_parameter(), mh.recv_string_parameter(), mh.recv_string_parameter())){
 		mh.send_code(Protocol::ANS_ACK);
 	}
 	else{
@@ -84,13 +86,13 @@ void handle_create_article(MessageHandler& mh){
 	mh.send_code(Protocol::ANS_END);
 }
 
-void handle_delete_article(MessageHandler& mh){
+void handle_delete_article(MessageHandler& mh, DatabaseInterface& db){
 // COM_DELETE_ART num_p num_p COM_END
 // ANS_DELETE_ART [ANS_ACK |
 // ANS_NAK [ERR_NG_DOES_NOT_EXIST | ERR_ART_DOES_NOT_EXIST]] ANS_END
 
 	mh.send_code(Protocol::ANS_DELETE_ART);
-	if(delete_article(mh.recv_int_parameter(), mh.recv_int_parameter())){
+	if(db.delete_article(mh.recv_int_parameter(), mh.recv_int_parameter())){
 		mh.send_code(Protocol::ANS_ACK);
 	}
 	else{
@@ -105,7 +107,7 @@ int main(int argc, char* argv[]){
 		cerr << "Usage: myserver port-number" << endl;
 		exit(1);
 	}
-
+	Database db;
 	int port = -1;
 	try {
 		port = stoi(argv[1]);
@@ -128,10 +130,10 @@ int main(int argc, char* argv[]){
 				Protocol nbr = static_cast<Protocol>(mh.recv_code());
 				string result = "";
         switch (nbr) {
-          case Protocol::COM_LIST_NG: get_list_newsgroups(mh); break;
-          case Protocol::COM_CREATE_NG: create_newsgroup(mh); break;
-          case Protocol::COM_DELETE_NG: delete_newsgroup(mh); break;
-					case Protocol::COM_LIST_ART: handle_list_articles(mh); break;
+          case Protocol::COM_LIST_NG: get_list_newsgroups(mh, db); break;
+          case Protocol::COM_CREATE_NG: create_newsgroup(mh, db); break;
+          case Protocol::COM_DELETE_NG: delete_newsgroup(mh, db); break;
+					case Protocol::COM_LIST_ART: handle_list_articles(mh, db); break;
           case Protocol::COM_CREATE_ART: break;
           case Protocol::COM_DELETE_ART: break;
           case Protocol::COM_GET_ART: break;
