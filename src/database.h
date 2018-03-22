77761d2e547c79200814d0346150d1849764f547
antongoransson <anton.goransson95@gmail.com>
2018-03-22 22:09:13 +0100

    Remove using from h file

--- src/database.h
+++ src/database.h
@@ -6,20 +6,16 @@
 #include <utility>
 #include <map>
 
-using std::string;
-using std::vector;
-using std::pair;
-
 class Database: public DatabaseInterface {
 public:
   Database();
-  vector<pair<int,string>> list_news_groups() override;
-  pair<vector<pair<int,string>>, bool> list_articles(int grpID) override;
-  bool create_news_group(string name) override;
-  bool create_article(int grpID, string name, string author, string text) override;
+  std::vector<std::pair<int, std::string>> list_news_groups() override;
+  std::pair<std::vector<std::pair<int, std::string>>, bool> list_articles(int grpID) override;
+  bool create_news_group(std::string name) override;
+  bool create_article(int grpID, std::string name, std::string author, std::string text) override;
   bool delete_news_group(int grpID) override;
   int delete_article(int grpID, int artID) override;
-  pair<Article, int> get_article(int grpID, int artID) override;
+  std::pair<Article, int> get_article(int grpID, int artID) override;
 private:
   map<int, NewsGroup> news_groups;
   map<int, map<int, Article>> articles;
