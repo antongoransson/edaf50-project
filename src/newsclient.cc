ebf4579f58a6861de1abc187cc7f1e7b57aa25df
antongoransson <anton.goransson95@gmail.com>
2018-03-27 23:45:36 +0200

    CHange from tabs to 2 spaces

--- src/newsclient.cc
+++ src/newsclient.cc
@@ -20,273 +20,271 @@ using std::numeric_limits;
 using std::streamsize;
 
 void error_message_handler(Protocol& res) {
-	switch (res) {
-		case Protocol::ERR_NG_DOES_NOT_EXIST:
-			cout << "Newsgroup does not exist" << endl;
-		 	break;
-		case Protocol::ERR_NG_ALREADY_EXISTS:
-			cout << "Newsgroup already exists" << endl;
-			break;
-		case Protocol::ERR_ART_DOES_NOT_EXIST:
-			cout << "Article does not exist" << endl;
-		 	break;
-		default:
-			cout << "Something went wrong" << endl;
-			break;
-	}
+  switch (res) {
+    case Protocol::ERR_NG_DOES_NOT_EXIST:
+      cout << "Newsgroup does not exist" << endl;
+      break;
+    case Protocol::ERR_NG_ALREADY_EXISTS:
+      cout << "Newsgroup already exists" << endl;
+      break;
+    case Protocol::ERR_ART_DOES_NOT_EXIST:
+      cout << "Article does not exist" << endl;
+      break;
+    default:
+      cout << "Something went wrong" << endl;
+      break;
+  }
 }
 
 int read_int_in() {
-	return 0;
+  return 0;
 }
 
 string read_string_in() {
-	return "abc";
+  return "abc";
 }
 
 void handle_list_newsgroups(MessageHandler& mh) {
-	mh.send_code(Protocol::COM_LIST_NG);
-	mh.send_code(Protocol::COM_END);
-	mh.recv_code();
-	int nbr = mh.recv_int_parameter();
-	if (nbr == 0 ) {
-		cout << "No groups exists use command: 2 to create one" << endl;
-	} else {
-		for (int i = 0; i != nbr; ++i) {
-			int grpID = mh.recv_int_parameter();
-			string name = mh.recv_string_parameter();
-			cout << "Group id: " << grpID << "\t   " << "Name: " << name << endl;
-		}
-	}
-	mh.recv_code();
+  mh.send_code(Protocol::COM_LIST_NG);
+  mh.send_code(Protocol::COM_END);
+  mh.recv_code();
+  int nbr = mh.recv_int_parameter();
+  if (nbr == 0 ) {
+    cout << "No groups exists use command: 2 to create one" << endl;
+  } else {
+    for (int i = 0; i != nbr; ++i) {
+      int grpID = mh.recv_int_parameter();
+      string name = mh.recv_string_parameter();
+      cout << "Group id: " << grpID << "\t   " << "Name: " << name << endl;
+    }
+  }
+  mh.recv_code();
 }
 
 void handle_create_newsgroups(MessageHandler& mh) {
-	string name;
-	cout << "Enter name of newsgroup: ";
-	cin.ignore();
-	getline(cin, name);
-	mh.send_code(Protocol::COM_CREATE_NG);
-	mh.send_string_parameter(name);
-	mh.send_code(Protocol::COM_END);
-	mh.recv_code();
-	Protocol res = mh.recv_code();
-	if (res == Protocol::ANS_ACK) {
-		cout << "Newsgroup with name: " << name << " created" << endl;
-	} else {
-		res = mh.recv_code();
-		error_message_handler(res);
-	}
-	mh.recv_code();
+  string name;
+  cout << "Enter name of newsgroup: ";
+  cin.ignore();
+  getline(cin, name);
+  mh.send_code(Protocol::COM_CREATE_NG);
+  mh.send_string_parameter(name);
+  mh.send_code(Protocol::COM_END);
+  mh.recv_code();
+  Protocol res = mh.recv_code();
+  if (res == Protocol::ANS_ACK) {
+    cout << "Newsgroup with name: " << name << " created" << endl;
+  } else {
+    res = mh.recv_code();
+    error_message_handler(res);
+  }
+  mh.recv_code();
 }
 
 void handle_delete_newsgroup(MessageHandler& mh) {
-	string name, temp;
-	int id;
-	cout << "Enter id of newsgroup to delete: ";
-	cin >> id;
-	mh.send_code(Protocol::COM_DELETE_NG);
-	mh.send_int_parameter(id);
-	mh.send_code(Protocol::COM_END);
-	mh.recv_code();
-	Protocol res = mh.recv_code();
-	if (res == Protocol::ANS_ACK) {
-		cout << "Newsgroup with id: " << id << " deleted" << endl;
-	} else {
-		res = mh.recv_code();
-		error_message_handler(res);
-	}
-	mh.recv_code();
+  string name, temp;
+  int id;
+  cout << "Enter id of newsgroup to delete: ";
+  cin >> id;
+  mh.send_code(Protocol::COM_DELETE_NG);
+  mh.send_int_parameter(id);
+  mh.send_code(Protocol::COM_END);
+  mh.recv_code();
+  Protocol res = mh.recv_code();
+  if (res == Protocol::ANS_ACK) {
+    cout << "Newsgroup with id: " << id << " deleted" << endl;
+  } else {
+    res = mh.recv_code();
+    error_message_handler(res);
+  }
+  mh.recv_code();
 }
 
 void handle_list_articles(MessageHandler& mh) {
-	int id;
-	cout << "Enter number of newsgroup: ";
-	cin >> id;
+  int id;
+  cout << "Enter number of newsgroup: ";
+  cin >> id;
 
-	mh.send_code(Protocol::COM_LIST_ART);
-	mh.send_int_parameter(id);
-	mh.send_code(Protocol::COM_END);
-	mh.recv_code();
-	Protocol res = mh.recv_code();
-	if (res == Protocol::ANS_ACK) {
-		int nbr = mh.recv_int_parameter();
-		if (nbr == 0 ) {
-			cout << "No articles exists in that group, use command: 5 to create one" << endl;
-		} else {
-			for (int i = 0; i != nbr; ++i) {
-				int grpID = mh.recv_int_parameter();
-				string name = mh.recv_string_parameter();
-				cout << "Article id:  " << grpID << "\t   " << "Name: " << name << endl;
-			}
-		}
-	} else {
-		res = mh.recv_code();
-		error_message_handler(res);
-	}
-	mh.recv_code();
+  mh.send_code(Protocol::COM_LIST_ART);
+  mh.send_int_parameter(id);
+  mh.send_code(Protocol::COM_END);
+  mh.recv_code();
+  Protocol res = mh.recv_code();
+  if (res == Protocol::ANS_ACK) {
+    int nbr = mh.recv_int_parameter();
+    if (nbr == 0 ) {
+    cout << "No articles exists in that group, use command: 5 to create one" << endl;
+    } else {
+      for (int i = 0; i != nbr; ++i) {
+      int grpID = mh.recv_int_parameter();
+      string name = mh.recv_string_parameter();
+      cout << "Article id:  " << grpID << "\t   " << "Name: " << name << endl;
+      }
+    }
+  } else {
+    res = mh.recv_code();
+    error_message_handler(res);
+  }
+  mh.recv_code();
 }
 
 
 
 void handle_create_article(MessageHandler& mh) {
-	string title, author, text, line	;
-	int id;
-	cout << "Enter id of newsgroup: ";
-	cin >> id;
+  string title, author, text, line	;
+  int id;
+  cout << "Enter id of newsgroup: ";
+  cin >> id;
 
-	cout << "Enter title of article: ";
-	cin.ignore();
-	getline(cin, title);
-	cout << "Enter author of article: ";
-	getline(cin, author);
-	cout << "Enter text of article (Press ctrl + D when finished): ";
-	while (getline(std::cin, line)) {
-		text += line + "\n";
-	}
-	cout << title << endl;
-	cout << author << endl;
+  cout << "Enter title of article: ";
+  cin.ignore();
+  getline(cin, title);
+  cout << "Enter author of article: ";
+  getline(cin, author);
+  cout << "Enter text of article (Press ctrl + D when finished): ";
+  while (getline(std::cin, line)) {
+    text += line + "\n";
+  }
+  cout << title << endl;
+  cout << author << endl;
 
-	cin.clear();
-	mh.send_code(Protocol::COM_CREATE_ART);
-	mh.send_int_parameter(id);
-	mh.send_string_parameter(title);
-	mh.send_string_parameter(author);
-	mh.send_string_parameter(text);
-	mh.send_code(Protocol::COM_END);
-	mh.recv_code();
-	Protocol res = mh.recv_code();
-	if (res == Protocol::ANS_ACK) {
-		cout << "Article with title: " << title << " created" << endl;
-	} else {
-		res = mh.recv_code();
-		error_message_handler(res);
-	}
-	mh.recv_code();
+  cin.clear();
+  mh.send_code(Protocol::COM_CREATE_ART);
+  mh.send_int_parameter(id);
+  mh.send_string_parameter(title);
+  mh.send_string_parameter(author);
+  mh.send_string_parameter(text);
+  mh.send_code(Protocol::COM_END);
+  mh.recv_code();
+  Protocol res = mh.recv_code();
+  if (res == Protocol::ANS_ACK) {
+    cout << "Article with title: " << title << " created" << endl;
+  } else {
+    res = mh.recv_code();
+    error_message_handler(res);
+  }
+  mh.recv_code();
 }
 
 void handle_delete_article(MessageHandler& mh) {
-	int grpID;
-	cout << "Enter id of the article's newsgroup : ";
-	cin >> grpID;
-	int artID;
-	cout << "Enter id of the article : ";
-	cin >> artID;
-	mh.send_code(Protocol::COM_DELETE_ART);
-	mh.send_int_parameter(grpID);
-	mh.send_int_parameter(artID);
-	mh.send_code(Protocol::COM_END);
-	mh.recv_code();
-	Protocol res = mh.recv_code();
-	if (res == Protocol::ANS_ACK) {
-		cout << "Article deleted" << endl;
-	} else {
-		res = mh.recv_code();
-		error_message_handler(res);
-	}
-	mh.recv_code();
+  int grpID;
+  cout << "Enter id of the article's newsgroup : ";
+  cin >> grpID;
+  int artID;
+  cout << "Enter id of the article : ";
+  cin >> artID;
+  mh.send_code(Protocol::COM_DELETE_ART);
+  mh.send_int_parameter(grpID);
+  mh.send_int_parameter(artID);
+  mh.send_code(Protocol::COM_END);
+  mh.recv_code();
+  Protocol res = mh.recv_code();
+  if (res == Protocol::ANS_ACK) {
+    cout << "Article deleted" << endl;
+  } else {
+    res = mh.recv_code();
+    error_message_handler(res);
+  }
+  mh.recv_code();
 }
 
 void handle_get_article(MessageHandler& mh) {
-	int grpID;
-	cout << "Enter id of the article's newsgroup : ";
-	cin >> grpID;
-	int artID;
-	cout << "Enter id of the article : ";
-	cin >> artID;
-	mh.send_code(Protocol::COM_GET_ART);
-	mh.send_int_parameter(grpID);
-	mh.send_int_parameter(artID);
-	mh.send_code(Protocol::COM_END);
-	mh.recv_code();
-	Protocol res = mh.recv_code();
-	if (res == Protocol::ANS_ACK) {
-		string title = mh.recv_string_parameter();
-		string author = mh.recv_string_parameter();
-		string text = mh.recv_string_parameter();
-		cout << "------------------------------------" << endl;
-		cout << "Title: " << title << endl << "Author: " << author << endl << text << endl;
-	} else {
-		res = mh.recv_code();
-		error_message_handler(res);
-	}
-	mh.recv_code();
+  int grpID;
+  cout << "Enter id of the article's newsgroup : ";
+  cin >> grpID;
+  int artID;
+  cout << "Enter id of the article : ";
+  cin >> artID;
+  mh.send_code(Protocol::COM_GET_ART);
+  mh.send_int_parameter(grpID);
+  mh.send_int_parameter(artID);
+  mh.send_code(Protocol::COM_END);
+  mh.recv_code();
+  Protocol res = mh.recv_code();
+  if (res == Protocol::ANS_ACK) {
+    string title = mh.recv_string_parameter();
+    string author = mh.recv_string_parameter();
+    string text = mh.recv_string_parameter();
+    cout << "------------------------------------" << endl;
+    cout << "Title: " << title << endl << "Author: " << author << endl << text << endl;
+  } else {
+    res = mh.recv_code();
+    error_message_handler(res);
+  }
+  mh.recv_code();
 }
 
 
 void list_instructions() {
-	cout << "1. List newsgroups" << endl;
-	cout << "2. Create newsgroup" << endl;
-	cout << "3. Delete newsgroup" << endl;
-	cout << "4. List articles" << endl;
-	cout << "5. Create an article" << endl;
-	cout << "6. Delete an article" << endl;
-	cout << "7. Get an article" << endl;
-	cout << "8. To list these instructions again" << endl;
-	cout << "0. To exit the program" << endl;
+  cout << "1. List newsgroups" << endl;
+  cout << "2. Create newsgroup" << endl;
+  cout << "3. Delete newsgroup" << endl;
+  cout << "4. List articles" << endl;
+  cout << "5. Create an article" << endl;
+  cout << "6. Delete an article" << endl;
+  cout << "7. Get an article" << endl;
+  cout << "8. To list these instructions again" << endl;
+  cout << "0. To exit the program" << endl;
 }
 
 int main(int argc, char* argv[]) {
-	if (argc != 3) {
-		cerr << "Usage: myclient host-name port-number" << endl;
-		exit(1);
-	}
-	int port = -1;
-	try {
-		port = stoi(argv[2]);
-	} catch (exception& e) {
-		cerr << "Wrong port number. " << e.what() << endl;
-		exit(1);
-	}
+  if (argc != 3) {
+    cerr << "Usage: myclient host-name port-number" << endl;
+    exit(1);
+  }
+  int port = -1;
+  try {
+    port = stoi(argv[2]);
+  } catch (exception& e) {
+    cerr << "Wrong port number. " << e.what() << endl;
+    exit(1);
+  }
 
-	Connection conn(argv[1], port);
-	if (!conn.isConnected()) {
-		cerr << "Connection attempt failed" << endl;
-		exit(1);
-	}
- 	cout << "Welcome to our news system!" << endl << "Avaible commands:" << endl;
-	list_instructions();
-	cout << "------------------------------------" << endl;
-	cout << "Enter command: ";
+  Connection conn(argv[1], port);
+  if (!conn.isConnected()) {
+    cerr << "Connection attempt failed" << endl;
+    exit(1);
+  }
+  cout << "Welcome to our news system!" << endl << "Avaible commands:" << endl;
+  list_instructions();
+  cout << "------------------------------------" << endl;
+  cout << "Enter command: ";
 
-	MessageHandler mh(conn);
-	int command;
-	while (true) {
-		if (cin >> command) {
-			try {
-				switch (command) {
-					case 1: handle_list_newsgroups(mh); break;
-					case 2: handle_create_newsgroups(mh); break;
-					case 3: handle_delete_newsgroup(mh); break;
-					case 4: handle_list_articles(mh); break;
-					case 5: handle_create_article(mh); break;
-					case 6: handle_delete_article(mh); break;
-					case 7: handle_get_article(mh); break;
-					case 8: list_instructions(); break;
-					case 0:
-						cout << "Exiting... Thanks for your visit!";
-						exit(0);
-					default:
-					 	cout << "Non existing command use on of the following:" << endl;
-						list_instructions();
-					break;
-
-				}
-				cout << "------------------------------------" << endl;
-				cout << "Enter new command: " ;
-
-			} catch (ConnectionClosedException&) {
-				cout << " no reply from server. Exiting." << endl;
-				exit(1);
-			}
-		} else {
-			cout << command;
-			cin.clear();
-			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			cout << "Invalid command, please choose one from the list: " << endl;
-			list_instructions();
-			cout << "------------------------------------" << endl;
-			cout << "Enter new command: " ;
-		}
-	}
+  MessageHandler mh(conn);
+  int command;
+  while (true) {
+    if (cin >> command) {
+      try {
+        switch (command) {
+          case 1: handle_list_newsgroups(mh); break;
+          case 2: handle_create_newsgroups(mh); break;
+          case 3: handle_delete_newsgroup(mh); break;
+          case 4: handle_list_articles(mh); break;
+          case 5: handle_create_article(mh); break;
+          case 6: handle_delete_article(mh); break;
+          case 7: handle_get_article(mh); break;
+          case 8: list_instructions(); break;
+          case 0:
+          cout << "Exiting... Thanks for your visit!";
+          exit(0);
+          default:
+          cout << "Non existing command use on of the following:" << endl;
+          list_instructions();
+          break;
+        }
+        cout << "------------------------------------" << endl;
+        cout << "Enter new command: " ;
+      } catch (ConnectionClosedException&) {
+        cout << " no reply from server. Exiting." << endl;
+        exit(1);
+      }
+    } else {
+      cout << command;
+      cin.clear();
+      cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      cout << "Invalid command, please choose one from the list: " << endl;
+      list_instructions();
+      cout << "------------------------------------" << endl;
+      cout << "Enter new command: " ;
+    }
+  }
 }
--- src/newsserver.cc
+++ src/newsserver.cc
@@ -25,161 +25,161 @@ using std::pair;
 using std::vector;
 
 void handle_list_articles(MessageHandler& mh, DatabaseInterface& db) {
-	 mh.send_code(Protocol::ANS_LIST_ART);
-	 int nbr = mh.recv_int_parameter();
-	 pair<vector<pair<int,string>>, bool> articles = db.list_articles(nbr);
-	 if (!articles.second) {
-		 	mh.send_code(Protocol::ANS_NAK);
-		  mh.send_code(Protocol::ERR_NG_DOES_NOT_EXIST);
-	 } else {
-		 mh.send_code(Protocol::ANS_ACK);
-		 vector<pair<int,string>>v = articles.first;
-		 int size = v.size();
-		 mh.send_int_parameter(size);
-		 for (auto p: v) {
-	 		mh.send_int_parameter(p.first);
-	 		mh.send_string_parameter(p.second);
-	 	}
-	 }
-	 mh.send_code(Protocol::ANS_END);
+  mh.send_code(Protocol::ANS_LIST_ART);
+  int nbr = mh.recv_int_parameter();
+  pair<vector<pair<int,string>>, bool> articles = db.list_articles(nbr);
+  if (!articles.second) {
+    mh.send_code(Protocol::ANS_NAK);
+    mh.send_code(Protocol::ERR_NG_DOES_NOT_EXIST);
+  } else {
+    mh.send_code(Protocol::ANS_ACK);
+    vector<pair<int,string>>v = articles.first;
+    int size = v.size();
+    mh.send_int_parameter(size);
+    for (auto p: v) {
+      mh.send_int_parameter(p.first);
+      mh.send_string_parameter(p.second);
+    }
+  }
+  mh.send_code(Protocol::ANS_END);
 }
 
 void get_list_newsgroups(MessageHandler& mh, DatabaseInterface& db) {
-	mh.send_code(Protocol::ANS_LIST_NG);
-	vector<pair<int, string>> v = db.list_news_groups();
-	int size = v.size();
-	mh.send_int_parameter(size);
-	for (auto p: v) {
-		mh.send_int_parameter(p.first);
-		mh.send_string_parameter(p.second);
-	}
-	mh.send_code(Protocol::ANS_END);
+  mh.send_code(Protocol::ANS_LIST_NG);
+  vector<pair<int, string>> v = db.list_news_groups();
+  int size = v.size();
+  mh.send_int_parameter(size);
+  for (auto p: v) {
+    mh.send_int_parameter(p.first);
+    mh.send_string_parameter(p.second);
+  }
+  mh.send_code(Protocol::ANS_END);
 }
 
 void handle_create_news_group(MessageHandler& mh, DatabaseInterface& db) {
-	mh.send_code(Protocol::ANS_CREATE_NG);
-	if (db.create_news_group(mh.recv_string_parameter())) {
-		mh.send_code(Protocol::ANS_ACK);
-	} else {
-		mh.send_code(Protocol::ANS_NAK);
-		mh.send_code(Protocol::ERR_NG_ALREADY_EXISTS);
-	}
-	mh.send_code(Protocol::ANS_END);
+  mh.send_code(Protocol::ANS_CREATE_NG);
+  if (db.create_news_group(mh.recv_string_parameter())) {
+    mh.send_code(Protocol::ANS_ACK);
+  } else {
+    mh.send_code(Protocol::ANS_NAK);
+    mh.send_code(Protocol::ERR_NG_ALREADY_EXISTS);
+  }
+  mh.send_code(Protocol::ANS_END);
 }
 
 void handle_delete_news_group(MessageHandler& mh, DatabaseInterface& db) {
-	mh.send_code(Protocol::ANS_DELETE_NG);
-	int grpID = mh.recv_int_parameter();
-	if (db.delete_news_group(grpID)) {
-		mh.send_code(Protocol::ANS_ACK);
-	}	else {
-		mh.send_code(Protocol::ANS_NAK);
-		mh.send_code(Protocol::ERR_NG_DOES_NOT_EXIST);
-	}
-	mh.send_code(Protocol::ANS_END);
+  mh.send_code(Protocol::ANS_DELETE_NG);
+  int grpID = mh.recv_int_parameter();
+  if (db.delete_news_group(grpID)) {
+    mh.send_code(Protocol::ANS_ACK);
+  } else {
+    mh.send_code(Protocol::ANS_NAK);
+    mh.send_code(Protocol::ERR_NG_DOES_NOT_EXIST);
+  }
+  mh.send_code(Protocol::ANS_END);
 }
 
 void handle_create_article(MessageHandler& mh, DatabaseInterface& db) {
-	mh.send_code(Protocol::ANS_CREATE_ART);
-	int grpID = mh.recv_int_parameter();
-	string title = mh.recv_string_parameter();
-	string author = mh.recv_string_parameter();
-	string text = mh.recv_string_parameter();
-	if (db.create_article(grpID, title, author, text)) {
-		mh.send_code(Protocol::ANS_ACK);
-	}	else {
-		mh.send_code(Protocol::ANS_NAK);
-		mh.send_code(Protocol::ERR_NG_ALREADY_EXISTS);
-	}
-	mh.send_code(Protocol::ANS_END);
+  mh.send_code(Protocol::ANS_CREATE_ART);
+  int grpID = mh.recv_int_parameter();
+  string title = mh.recv_string_parameter();
+  string author = mh.recv_string_parameter();
+  string text = mh.recv_string_parameter();
+  if (db.create_article(grpID, title, author, text)) {
+    mh.send_code(Protocol::ANS_ACK);
+  } else {
+    mh.send_code(Protocol::ANS_NAK);
+    mh.send_code(Protocol::ERR_NG_ALREADY_EXISTS);
+  }
+  mh.send_code(Protocol::ANS_END);
 }
 
 void handle_delete_article(MessageHandler& mh, DatabaseInterface& db) {
-	mh.send_code(Protocol::ANS_DELETE_ART);
-	int grpID = mh.recv_int_parameter();
-	int artID = mh.recv_int_parameter();
-	int delete_int = db.delete_article(grpID, artID);
-	if (delete_int == 1) {
-		mh.send_code(Protocol::ANS_ACK);
-	}	else if (delete_int == 2) {
-		mh.send_code(Protocol::ANS_NAK);
-		mh.send_code(Protocol::ERR_ART_DOES_NOT_EXIST);
-	} else {
-		mh.send_code(Protocol::ANS_NAK);
-		mh.send_code(Protocol::ERR_NG_DOES_NOT_EXIST);
-	}
-	mh.send_code(Protocol::ANS_END);
+  mh.send_code(Protocol::ANS_DELETE_ART);
+  int grpID = mh.recv_int_parameter();
+  int artID = mh.recv_int_parameter();
+  int delete_int = db.delete_article(grpID, artID);
+  if (delete_int == 1) {
+    mh.send_code(Protocol::ANS_ACK);
+  } else if (delete_int == 2) {
+    mh.send_code(Protocol::ANS_NAK);
+    mh.send_code(Protocol::ERR_ART_DOES_NOT_EXIST);
+  } else {
+    mh.send_code(Protocol::ANS_NAK);
+    mh.send_code(Protocol::ERR_NG_DOES_NOT_EXIST);
+  }
+  mh.send_code(Protocol::ANS_END);
 }
 
 void handle_get_article(MessageHandler& mh, DatabaseInterface& db) {
-	mh.send_code(Protocol::ANS_GET_ART);
-	int grpID = mh.recv_int_parameter();
-	int artID = mh.recv_int_parameter();
-	pair<Article, int> p =  db.get_article(grpID, artID);
-	if (p.second == 1) {
-		mh.send_code(Protocol::ANS_ACK);
-		Article a = p.first;
-		mh.send_string_parameter(a.get_title());
-		mh.send_string_parameter(a.get_author());
-		mh.send_string_parameter(a.get_text());
-	}	else {
-		mh.send_code(Protocol::ANS_NAK);
-		if (p.second == 3) {
-			mh.send_code(Protocol::ERR_NG_DOES_NOT_EXIST);
-		} else {
-			mh.send_code(Protocol::ERR_ART_DOES_NOT_EXIST);
-		}
-	}
-	mh.send_code(Protocol::ANS_END);
+  mh.send_code(Protocol::ANS_GET_ART);
+  int grpID = mh.recv_int_parameter();
+  int artID = mh.recv_int_parameter();
+  pair<Article, int> p =  db.get_article(grpID, artID);
+  if (p.second == 1) {
+    mh.send_code(Protocol::ANS_ACK);
+    Article a = p.first;
+    mh.send_string_parameter(a.get_title());
+    mh.send_string_parameter(a.get_author());
+    mh.send_string_parameter(a.get_text());
+  } else {
+    mh.send_code(Protocol::ANS_NAK);
+    if (p.second == 3) {
+      mh.send_code(Protocol::ERR_NG_DOES_NOT_EXIST);
+    } else {
+      mh.send_code(Protocol::ERR_ART_DOES_NOT_EXIST);
+    }
+  }
+  mh.send_code(Protocol::ANS_END);
 }
 
 int main(int argc, char* argv[]) {
-	if (argc != 2) {
-		cerr << "Usage: myserver port-number" << endl;
-		exit(1);
-	}
-	Database ddb;
-	DiskDatabase db;
-	int port = -1;
-	try {
-		port = stoi(argv[1]);
-	} catch (exception& e) {
-		cerr << "Wrong port number. " << e.what() << endl;
-		exit(1);
-	}
+  if (argc != 2) {
+    cerr << "Usage: myserver port-number" << endl;
+    exit(1);
+  }
+  Database ddb;
+  DiskDatabase db;
+  int port = -1;
+  try {
+    port = stoi(argv[1]);
+  } catch (exception& e) {
+    cerr << "Wrong port number. " << e.what() << endl;
+    exit(1);
+  }
 
-	Server server(port);
-	if (!server.isReady()) {
-		cerr << "Server initialization error." << endl;
-		exit(1);
-	}
+  Server server(port);
+  if (!server.isReady()) {
+    cerr << "Server initialization error." << endl;
+    exit(1);
+  }
 
-	while (true) {
-		auto conn = server.waitForActivity();
-		if (conn != nullptr) {
-			try {
-				MessageHandler mh(conn);
-				Protocol nbr = mh.recv_code();
+  while (true) {
+    auto conn = server.waitForActivity();
+    if (conn != nullptr) {
+      try {
+        MessageHandler mh(conn);
+        Protocol nbr = mh.recv_code();
         switch (nbr) {
           case Protocol::COM_LIST_NG: get_list_newsgroups(mh, db); break;
           case Protocol::COM_CREATE_NG: handle_create_news_group(mh, db); break;
           case Protocol::COM_DELETE_NG: handle_delete_news_group(mh, db); break;
-					case Protocol::COM_LIST_ART: handle_list_articles(mh, db); break;
+          case Protocol::COM_LIST_ART: handle_list_articles(mh, db); break;
           case Protocol::COM_CREATE_ART: handle_create_article(mh, db); break;
           case Protocol::COM_DELETE_ART: handle_delete_article(mh, db); break;
           case Protocol::COM_GET_ART: handle_get_article(mh, db); break;
           case Protocol::COM_END: break;
           default: break;
         }
-				mh.recv_code();
-			} catch (ConnectionClosedException&) {
-				server.deregisterConnection(conn);
-				cout << "Client closed connection" << endl;
-			}
-		} else {
-			conn = make_shared<Connection>();
-			server.registerConnection(conn);
-			cout << "New client connects" << endl;
-		}
-	}
+        mh.recv_code();
+      } catch (ConnectionClosedException&) {
+      server.deregisterConnection(conn);
+      cout << "Client closed connection" << endl;
+      }
+    } else {
+      conn = make_shared<Connection>();
+      server.registerConnection(conn);
+      cout << "New client connects" << endl;
+    }
+  }
 }
