68d5a93d491d086875a9f8579b3999e206b56f7f
antongoransson <anton.goransson95@gmail.com>
2018-03-27 15:13:38 +0200

    Add errormessagehandler

--- src/newsclient.cc
+++ src/newsclient.cc
@@ -16,6 +16,22 @@ using std::string;
 using std::exception;
 using std::stoi;
 
+void error_message_handler(Protocol& res) {
+	switch (res) {
+		case Protocol::ERR_NG_DOES_NOT_EXIST:
+			cout << "Newsgroup does not exist" << endl;
+		 	break;
+		case Protocol::ERR_NG_ALREADY_EXISTS:
+			cout << "Newsgroup already exists" << endl;
+			break;
+		case Protocol::ERR_ART_DOES_NOT_EXIST:
+			cout << "Article does not exist" << endl;
+		 	break;
+		default:
+			cout << "Something went wrong" << endl;
+			break;
+	}
+}
 void handle_list_newsgroups(MessageHandler& mh) {
 	mh.send_code(Protocol::COM_LIST_NG);
 	mh.send_code(Protocol::COM_END);
@@ -37,7 +53,7 @@ void handle_create_newsgroups(MessageHandler& mh) {
 	string name, temp;
 	cout << "Enter name of newsgroup: ";
 	cin.ignore();
-	getline(cin,name);
+	getline(cin, name);
 	mh.send_code(Protocol::COM_CREATE_NG);
 	mh.send_string_parameter(name);
 	mh.send_code(Protocol::COM_END);
@@ -46,7 +62,27 @@ void handle_create_newsgroups(MessageHandler& mh) {
 	if (res == Protocol::ANS_ACK) {
 		cout << "Newsgroup with name: " << name << " created" << endl;
 	} else {
-		cout << "Newsgroup with name: " << name << "alrady exists" << endl;
+		res = mh.recv_code();
+		error_message_handler(res);
+	}
+	mh.recv_code();
+}
+
+void handle_delete_newsgroup(MessageHandler& mh) {
+	string name, temp;
+	int id;
+	cout << "Enter id of newsgroup to delete: ";
+	cin >> id;
+	mh.send_code(Protocol::COM_DELETE_NG);
+	mh.send_int_parameter(id);
+	mh.send_code(Protocol::COM_END);
+	mh.recv_code();
+	Protocol res = mh.recv_code();
+	if (res == Protocol::ANS_ACK) {
+		cout << "Newsgroup with id: " << id << " deleted" << endl;
+	} else {
+		res = mh.recv_code();
+		error_message_handler(res);
 	}
 	mh.recv_code();
 }
@@ -82,6 +118,7 @@ int main(int argc, char* argv[]) {
 	cout << "7. Get an article" << endl;
 	cout << "8. To list these instructions again" << endl;
 	cout << "------------------------------------" << endl;
+	cout << "Enter command: ";
 
 	int command;
 	while (cin >> command) {
@@ -89,7 +126,7 @@ int main(int argc, char* argv[]) {
 			switch (command) {
 				case 1: handle_list_newsgroups(mh); break;
 				case 2: handle_create_newsgroups(mh); break;
-				case 3: break;
+				case 3: handle_delete_newsgroup(mh); break;
 				case 4: break;
 				case 5: break;
 				case 6: break;
@@ -99,6 +136,7 @@ int main(int argc, char* argv[]) {
 
 			}
 			cout << "------------------------------------" << endl;
+			cout << "Enter command: " ;
 			// writeNumber(conn, nbr);
 			// string reply = readString(conn);
 			// cout << " " << reply << endl;
